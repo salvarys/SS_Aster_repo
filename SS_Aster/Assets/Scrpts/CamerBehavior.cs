c7ebefc52a7828747b5f4afdeea1ca06a26b857f
salvarys <123325469+salvarys@users.noreply.github.com>
2024-09-17 11:25:54 -0700

    Update CamerBehavior.cs

--- SS_Aster/Assets/Scrpts/CamerBehavior.cs
+++ SS_Aster/Assets/Scrpts/CamerBehavior.cs
@@ -4,18 +4,14 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {
-  
     public Vector3 camOffset = new Vector3(0f, 1.2f, -2.6f);
-
 
     private Transform target;
 
     void Start()
     {
-
         target = GameObject.Find("Player").transform;
     }
-
 
     void LateUpdate()
     {
