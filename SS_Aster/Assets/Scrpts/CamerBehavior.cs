f03b9003d929bf47d20b7c57ec52f4c62c547b50
salvarys <123325469+salvarys@users.noreply.github.com>
2024-09-17 11:19:29 -0700

    Update Build

--- SS_Aster/Assets/Scrpts/CamerBehavior.cs
+++ SS_Aster/Assets/Scrpts/CamerBehavior.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamerBehavior : MonoBehaviour
+public class CameraBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+  
+    public Vector3 camOffset = new Vector3(0f, 1.2f, -2.6f);
+
+
+    private Transform target;
+
     void Start()
     {
-        
+
+        target = GameObject.Find("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void LateUpdate()
     {
-        
+        this.transform.position = target.TransformPoint(camOffset);
+
+        this.transform.LookAt(target);
+
+
     }
 }
--- SS_Aster/Assets/Scrpts/PlayerBehavior.cs
+++ SS_Aster/Assets/Scrpts/PlayerBehavior.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Players limits
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 75f;
+    public float jumpVelocity = 5f;
+    public float distanceToGround = 0.1f;
+    public LayerMask groundLayer;
+
+    //PLayer's Components
+    private bool bo;
+    private float vInput;
+    private float hInput;
+    private Rigidbody _rb;
+    private CapsuleCollider _col;
+
+    private int collectedPickUp;
+    public int _itemsCollected = 0;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+
+        _col = GetComponent<CapsuleCollider>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        // Player's Movement
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+
+
+        hInput = Input.GetAxis("Horizontal")
+            * rotateSpeed;
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            bo = true;
+        }
+
+
     }
+
+    //Player Phyisics 
+    void FixedUpdate()
+    {
+        if (bo)
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+            bo = false;
+        }
+
+
+        Vector3 rotation = Vector3.up * hInput;
+
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+
+        _rb.MoveRotation(_rb.rotation * angleRot);
+
+    }
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x,
+            _col.bounds.min.y, _col.bounds.center.z);
+
+        bool grounded = Physics.CheckCapsule(_col.bounds.center,
+                                              capsuleBottom,
+                                              distanceToGround,
+                                              groundLayer,
+                                              QueryTriggerInteraction.Ignore);
+
+        return grounded;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            collectedPickUp++;
+            Destroy(other.gameObject);
+            _itemsCollected++;
+        }
+    }
+
 }
